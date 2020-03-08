e62d4a8ab7606838ae02f93ab14324db2522c763
Fuks27 <37192741+Fuks27@users.noreply.github.com>
2020-03-08 22:59:27 +0100

    Add files via upload

---
+++ aha.js
@@ -0,0 +1,14 @@
+
+ function openPhoto(evt, photoName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(photoName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
